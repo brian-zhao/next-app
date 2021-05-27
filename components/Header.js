81bcb24899eaf3175942133de967ac7450179828
Brian Zhao <zhaoyubrian@gmail.com>
2021-05-27 23:27:56 +1000

    bring more dao article and added link to subsite.

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,18 @@ import Link from "next/link";
 export default function Header() {
   return (
     <nav>
-      <h1>Next'Blog</h1>
+      <h1>
+        <Link href="/">
+          <a>BZ</a>
+        </Link>
+      </h1>
       <div>
+        <Link href="http://learn.brianzhao.xyz/">
+          <a>Learn</a>
+        </Link>
+        <Link href="http://market.brianzhao.xyz/">
+          <a>ASX Market</a>
+        </Link>
         <Link href="/">
           <a>Blog</a>
         </Link>
