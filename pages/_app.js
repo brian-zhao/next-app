23db9e9ca840e4056445ce979d2fd08449319e1b
Brian Zhao <zhaoyubrian@gmail.com>
2021-07-10 11:33:23 +1000

    mdx code highlight and google analytics support.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import Layout from "../components/Layout";
+import CodeBlock from '../components/CodeBlock';
+import {MDXProvider} from '@mdx-js/react';
+
+const components = {
+  pre: props => <div {...props} />,
+  code: CodeBlock
+}
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout pageTitle="Blog" description="My Personal Blog">
-      <Component {...pageProps} />
+      <MDXProvider components={components}>
+          <Component {...pageProps} />
+      </MDXProvider>
     </Layout>
   );
 }
